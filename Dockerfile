cd7559fc92fc9d3d646d4a133fe2f72ebbc3d544
ProfoundlyParker <parkerramey@yahoo.com>
2024-10-29 13:31:49 -0400

    Setting up Docker for back-end example

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:20.10.0
+FROM node:20.13.0
 
 WORKDIR /usr/src/smartbrain-api-exercise
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser'); // latest version of exressJS now com
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const morgan = require('morgan');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -16,8 +17,6 @@ const db = knex({
 });
 
 const app = express();
-
-
 app.use(cors())
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
